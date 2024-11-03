1d2bab966604a57b13d871bc5d18825a55ddf09f
YJ Soon <yjsoon@tinkertanker.com>
2024-11-03 22:05:47 +0800

    Finished 8.2

--- Todo/ContentView.swift
+++ Todo/ContentView.swift
@@ -9,16 +9,25 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @State private var todos = [Todo(title: "Feed the cat"),
+    @State private var todos = [Todo(title: "Feed the cat", isCompleted: true),
                                 Todo(title: "Play with cat"),
                                 Todo(title: "Get allergies"),
                                 Todo(title: "Run away from cat"),
                                 Todo(title: "Get a new cat")]
     
     var body: some View {
-        List {
-            ForEach(todos) { todo in
-                Text(todo.title)
+        NavigationStack {
+            List {
+                ForEach($todos) { $todo in
+                    HStack {
+                        Image(systemName: todo.isCompleted ? "checkmark.circle.fill" : "circle")
+                            .onTapGesture {
+                                todo.isCompleted.toggle()
+                            }
+                        Text(todo.title)
+                            .strikethrough(todo.isCompleted)
+                    }
+                }
             }
         }
     }
