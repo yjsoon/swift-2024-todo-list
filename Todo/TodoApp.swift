c3bdb6a85cb5eb20e27cb2af34a4c92ec1a8d7c7
YJ Soon <yjsoon@tinkertanker.com>
2024-11-03 21:56:34 +0800

    Initial Commit

---
+++ Todo/TodoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoApp.swift
+//  Todo
+//
+//  Created by YJ Soon on 3/11/24.
+//
+
+import SwiftUI
+
+@main
+struct TodoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
